9ec044b66f9144a34d22d60bde7e6baff9aa8189
yacyam <119266083+yacyam@users.noreply.github.com>
2023-01-10 18:34:43 -0500

    basic template done

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean:true,
+    assetModuleFilename: '[name][ext]'
+  },
+  devServer: {
+    static: {
+        directory: path.resolve(__dirname, 'dist')
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+        {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(gif|png|webp|jpe?g)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: 'assets/images/'
+              }
+            }
+          ]
+        },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        title: 'Weather App',
+        filename: 'index.html',
+        template: 'src/template.html'
+        
+    })
+  ]
+};
